c7a5fef71240ab084446b4c9be877ac1a1eb7246
levimendes <levi.silva.mendes@gmail.com>
2016-04-17 14:20:03 -0300

    - commit test

--- app/src/main/java/br/com/levimendes/teste/mvp/BasicView.java
+++ app/src/main/java/br/com/levimendes/teste/mvp/BasicView.java
@@ -6,6 +6,7 @@ import android.view.View;
  * Created by Levi on 17/04/2016.
  */
 public interface BasicView {
+    //teste
     void showSnack(View view, int idMsg);
     void showToast(int idMsg);
 }
