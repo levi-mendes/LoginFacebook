976ab21921a9fbe629badeab37b8e95e33e19cdd
429317980 <levi.silva.mendes@gmail.com>
2016-12-12 19:07:30 -0200

    implementação dos métodos da BasicView

--- app/src/main/java/br/com/levimendes/teste/util/BaseActivity.java
+++ app/src/main/java/br/com/levimendes/teste/util/BaseActivity.java
@@ -1,10 +1,28 @@
 package br.com.levimendes.teste.util;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import br.com.levimendes.teste.mvp.contracts.BasicView;
+import static br.com.levimendes.teste.util.SnackUtil.showSnackLong;
+import static br.com.levimendes.teste.util.ToastUtil.showLong;
 
 /**
  * Created by 809778 on 05/07/2016.
  */
-public class BaseActivity extends AppCompatActivity {
+public class BaseActivity extends AppCompatActivity implements BasicView {
 
+    @Override
+    public void showSnack(View view, int idMsg) {
+        showSnackLong(getCurrentFocus(), getString(idMsg));
+    }
+
+    @Override
+    public void showToast(int idMsg) {
+        showLong(this, getString(idMsg));
+    }
+
+    @Override
+    public void showToast(String msg) {
+        showLong(this, msg);
+    }
 }
