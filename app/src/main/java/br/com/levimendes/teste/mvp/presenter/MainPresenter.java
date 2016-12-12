734fd1d8b8c4dbf8daf95da54d43d482c1f76e86
429317980 <levi.silva.mendes@gmail.com>
2016-12-12 19:07:02 -0200

    refactoring

--- app/src/main/java/br/com/levimendes/teste/mvp/presenter/MainPresenter.java
+++ app/src/main/java/br/com/levimendes/teste/mvp/presenter/MainPresenter.java
@@ -14,30 +14,30 @@ import com.google.gson.reflect.TypeToken;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import java.util.List;
 
-import java.util.ArrayList;
 import br.com.levimendes.teste.bean.Post;
-import br.com.levimendes.teste.mvp.contracts.MainActivityView;
+import br.com.levimendes.teste.mvp.contracts.MainActivityMVP;
 
 /**
  * Created by Levi on 16/04/2016.
  */
-public class MainPresenter {
+public class MainPresenter implements MainActivityMVP.Presenter {
 
-    private MainActivityView mView;
+    private MainActivityMVP.View mView;
 
-    public MainPresenter(MainActivityView view) {
+    public MainPresenter(MainActivityMVP.View view) {
         mView = view;
     }
 
-    private ArrayList<Post> graphResponseToPostList(GraphResponse response) {
+    private List<Post> graphResponseToPostList(GraphResponse response) {
         if (response != null) {
 
             try {
                 JSONArray jsonObject = response.getJSONObject().getJSONArray("data");
 
                 Gson gson = new Gson();
-                ArrayList<Post> posts = gson.fromJson(jsonObject.toString(), new TypeToken<ArrayList<Post>>(){}.getType());
+                List<Post> posts = gson.fromJson(jsonObject.toString(), new TypeToken<List<Post>>(){}.getType());
                 return posts;
 
             } catch (JSONException e) {
@@ -51,45 +51,20 @@ public class MainPresenter {
 
     private GraphRequest.Callback callback(LoginResult loginResult) {
         GraphRequest.Callback callback = response -> {
-                ArrayList<Post> posts = graphResponseToPostList(response);
+                List<Post> posts = graphResponseToPostList(response);
                 mView.launchTimelineActivity(posts, loginResult.getAccessToken());
         };
 
         return callback;
     }
+
+    @Override
     public FacebookCallback<LoginResult> facebookCallbackLogin() {
         FacebookCallback<LoginResult> retorno = new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-
-                //TODO criar deserializer para formatar o código
-                //busca timeline
                 GraphRequest graphRequest = new GraphRequest(AccessToken.getCurrentAccessToken(), "me/feed", null, HttpMethod.GET, callback(loginResult));
                 graphRequest.executeAsync();
-
-//                //TODO criar deserializer para formatar o código
-//                //busca timeline
-//                GraphRequest graphRequest = new GraphRequest(AccessToken.getCurrentAccessToken(), "me/feed", null, HttpMethod.GET,
-//                        new GraphRequest.Callback() {
-//                            public void onCompleted(GraphResponse response) {
-//
-//                                if (response != null) {
-//
-//                                    try {
-//                                        JSONArray jsonObject = response.getJSONObject().getJSONArray("data");
-//
-//                                        Gson gson = new Gson();
-//                                        ArrayList<Post> posts = gson.fromJson(jsonObject.toString(), new TypeToken<ArrayList<Post>>(){}.getType());
-//                                        mView.launchTimelineActivity(posts, loginResult.getAccessToken());
-//
-//                                    } catch (JSONException e) {
-//                                        Log.e("onCompleted", e.getMessage(), e);
-//                                        mView.showToast(e.getMessage());
-//                                    }
-//                                }
-//                            }
-//                    });
-//                graphRequest.executeAsync();
             }
 
             @Override
@@ -122,46 +97,4 @@ public class MainPresenter {
         graphRequest.setParameters(param);
         graphRequest.executeAsync();
     }
-
-
-    /*public FacebookCallback<LoginResult> facebookCallbackLogin() {
-        return new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(final LoginResult loginResult) {
-                //TODO criar deserializer para formatar o código
-                //busca timeline
-                new GraphRequest(AccessToken.getCurrentAccessToken(),
-                        "me/feed",
-                        null,
-                        HttpMethod.GET,
-                        new GraphRequest.Callback() {
-
-                            public void onCompleted(GraphResponse response) {
-                                if (response != null) {
-
-                                    try {
-                                        JSONArray jsonObject = response.getJSONObject().getJSONArray("data");
-
-                                        Gson gson = new Gson();
-                                        ArrayList<Post> posts = gson.fromJson(jsonObject.toString(), new TypeToken<ArrayList<Post>>(){}.getType());
-                                        mView.launchTimelineActivity(posts, loginResult.getAccessToken());
-
-                                    } catch (JSONException e) {
-                                        Log.e("onCompleted", e.getMessage(), e);
-                                        mView.showToast(e.getMessage());
-                                    }
-                                }
-                            }
-                        }).executeAndWait();
-            }
-
-            @Override
-            public void onCancel() {}
-
-            @Override
-            public void onError(FacebookException exception) {
-                Log.e("onError", exception.getMessage(), exception);
-            }
-        };
-    }*/
 }
