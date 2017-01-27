dc642de3159d4ae5cd4ff42b7bfb420ea4492840
429317980 <levi.silva.mendes@gmail.com>
2017-01-27 13:29:41 -0200

    classe usada para exibir o perfil do amigo selecionado

---
+++ app/src/main/java/br/com/levimendes/teste/activities/ProfileActivity.java
@@ -0,0 +1,15 @@
+package br.com.levimendes.teste.activities;
+
+import android.os.Bundle;
+
+import br.com.levimendes.teste.R;
+import br.com.levimendes.teste.util.BaseActivity;
+
+public class ProfileActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
